dcb2d69a9d05a87ee740f374e6a96b0989caf65b
pwmurphy89 <pwmurphy89@gmail.com>
2018-09-10 15:25:31 -0500

    basic database and faker factory built

---
+++ website/management/commands/faker_factory.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+seeder = Seed.seeder()
+import random
+from datetime import datetime
+from django.contrib.auth.models import User
+from website.models import Shift_Model, Table_Model
+from django.contrib.auth.hashers import make_password
+
+def random_date():
+    year = 2018
+    month = random.randint(1, 9)
+    day = random.randint(1, 28)
+    date = datetime(year, month, day)
+    return date
+def food():
+    return random.randint(10, 200)
+def drink():
+    return random.randint(10, 125)
+def guests():
+    return random.randint(1, 10)
+def tip():
+    return random.randint(12, 25)
+
+class Command(BaseCommand):
+    """Allows command line integration for faker_factory.py"""
+
+    def handle(self, *args, **options):
+        """uses faker to generate fake data.  First arg = model, second arg = number of entries to create"""
+
+        # the number argument is the total num of rows you want created
+
+        seeder.add_entity(User, 10, {
+            'password': lambda x: make_password("password")
+        })
+        seeder.add_entity(Shift_Model, 12, {
+            'date': lambda x: random_date()
+        })
+        seeder.add_entity(Table_Model, 12, {
+            'food_sales': lambda x: food(),
+            'drink_sales': lambda x: drink(),
+            'guests_number': lambda x: guests(),
+            'tip_percentage': lambda x: tip()
+        })
+
+
+        inserted_pks = seeder.execute()
--- website/models.py
+++ website/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import datetime, timedelta
+import random
+
+def default_start_time():
+    now = datetime.now()
+    start = now.replace(hour=17, minute=0, second=0, microsecond=0)
+    return start
+def default_end_time():
+    now = datetime.now()
+    end = now.replace(hour=22, minute=0, second=0, microsecond=0)
+    return end
 
 # Create your models here.
 class Product(models.Model):
@@ -11,3 +22,34 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     quantity = models.IntegerField()
+
+class Shift_Model(models.Model):
+    server = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    date = models.DateField()
+    time_in = models.TimeField(default=default_start_time)
+    time_out = models.TimeField(default=default_end_time)
+
+class Table_Model(models.Model):
+    shift = models.ForeignKey(Shift_Model, on_delete=models.CASCADE, default='')
+    food_sales = models.IntegerField()
+    drink_sales = models.IntegerField()
+    guests_number = models.IntegerField()
+    tip_percentage = models.IntegerField()
+    @property
+    def total(self):
+        return sum(self.food_sales, self.drink_sales)
+
+
+
+
+# class Departments_Model(models.Model):
+#     """
+#         This model holds the data for Bangazon Departments_Model.
+#         fields:
+#             name - string. name of the department. max length: 20.
+#     """
+
+#     name = models.CharField(max_length=20)
+
+#     def __str__(self):
+#         return self.name
